5bd0f3acf92aed59b51f0300da47d31f285cd1ee
usman <usmanaamirbs2022@gmail.com>
2024-08-25 07:33:31 +0000

    class RentalSystem added

--- Proj.java
+++ Proj.java
@@ -88,8 +88,72 @@ class Rental {
     }
 }
 
-class Proj {
-    public static void main(String[] args) {
-        System.out.println("Usman");
+class RentalSystem {
+    private List<Car> cars = new ArrayList<>();
+    private List<Rental> rentals = new ArrayList<>();
+
+    public RentalSystem() {
+        cars.add(new Car("C1", "Toyota", "Camry", 50.0));
+        cars.add(new Car("C2", "Honda", "Civic", 45.0));
+        cars.add(new Car("C3", "Ford", "Mustang", 70.0));
+        cars.add(new Car("C4","Audi","E-tron", 65.0));
     }
+
+    public void viewAvailableCars() {
+        System.out.println("Available Cars:");
+        for (Car car : cars) {
+            if (car.isAvailable()) {
+                System.out.println("ID: " + car.getCarId() + ", Brand: " + car.getBrand() + ", Model: " + car.getModel() + ", Price/Day: $" + car.calculatePrice(1));
+            }
+        }
+    }
+
+    public void rentCar(String carId, Customer customer, int days) {
+        for (Car car : cars) {
+            if (car.getCarId().equals(carId) && car.isAvailable()) {
+                car.rentStart();
+                Rental rental = new Rental(car, customer, days);
+                rentals.add(rental);
+                System.out.println(customer.getName() + " has rented the " + car.getBrand() + " " + car.getModel() + " for " + days + " days.");
+                return;
+            }
+        }
+        System.out.println("Car with ID " + carId + " is not available.");
+    }
+
+    public void extendRental(String carId, int additionalDays) {
+        for (Rental rental : rentals) {
+            if (rental.getCar().getCarId().equals(carId)) {
+                int newDays = rental.getDays() + additionalDays;
+                System.out.println("Rental for car " + rental.getCar().getBrand() + " " + rental.getCar().getModel() + " extended by " + additionalDays + " days. Total rental days: " + newDays);
+                return;
+            }
+        }
+        System.out.println("No rental found for car with ID " + carId);
+    }
+
+    public void viewRentalHistory(Customer customer) {
+        System.out.println("Rental History for " + customer.getName() + ":");
+        for (Rental rental : rentals) {
+            if (rental.getCustomer().getCustomerId().equals(customer.getCustomerId())) {
+                System.out.println("Car: " + rental.getCar().getBrand() + " " + rental.getCar().getModel() + ", Days Rented: " + rental.getDays());
+            }
+        }
+    }
+
+    public void processPayment(Rental rental) {
+        double totalAmount = rental.getCar().calculatePrice(rental.getDays());
+        System.out.println("Processing payment for " + rental.getCustomer().getName() + ":");
+        System.out.println("Car: " + rental.getCar().getBrand() + " " + rental.getCar().getModel());
+        System.out.println("Total Amount: $" + totalAmount);
+       
+    }
+    
+    public List<Rental> getRentals() {
+        return rentals;
+    }
+}
+
+public class Proj {
+    
 }
